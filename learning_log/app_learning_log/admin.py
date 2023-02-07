675552959391a951e097a1a5ec4520f823a4889a
Artemii Dovzhenko <rockenny@ymail.com>
2023-02-07 21:40:53 +0300

    add Entry model add EnrtyAdmin

--- learning_log/app_learning_log/admin.py
+++ learning_log/app_learning_log/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Topic
+from .models import Topic, Entry
 
 
 class TopicAdmin(admin.ModelAdmin):
     list_display = ('text', 'date_added')
 
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'text', 'date_add')
 
-admin.site.register(Topic)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Entry, EntryAdmin)
